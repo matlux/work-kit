a7317064d53d12110264cd60c38b486b66fc1135
Mathieu Gauthron <mathieu.gauthron@credit-suisse.com>
2013-08-08 00:23:37 -0700

    added project and basedir

--- tools/maven/hello-plugin-example/src/main/java/sample/plugin/GreetingMojo.java
+++ tools/maven/hello-plugin-example/src/main/java/sample/plugin/GreetingMojo.java
@@ -2,14 +2,22 @@ package sample.plugin;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.project.MavenProject;
 
 /**
  * Echos an object string to the output screen.
+ * Test it by running:
+ *      mvn sample.plugin:hello-maven-plugin:echo -Decho.message="The Eagle has Landed"
  * @goal echo
  * @requiresProject false
  */
 public class GreetingMojo extends AbstractMojo
 {
+
+
+
+    /** @parameter default-value="${project}" */
+    private MavenProject project;
 
     /**
      * Any Object to print out.
@@ -20,5 +28,6 @@ public class GreetingMojo extends AbstractMojo
     public void execute() throws MojoExecutionException
     {
         getLog().info( message.toString() );
+        getLog().info( "basedir=" + project.getBasedir() );
     }
 }
